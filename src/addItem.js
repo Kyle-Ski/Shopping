11ee612123b7bf2c0dff1b06cb24af0a8d5b138e
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-11 08:28:59 -0600

    commented out the console.log button

--- src/addItem.js
+++ src/addItem.js
@@ -15,7 +15,7 @@ const AddItem = ({products, submit, selectQuantity, showState, setProduct}) => {
             <label htmlFor="quantity">Quantity</label>
             <input onChange={selectQuantity} type="number" min="0" className="form-control" id="quantity" placeholder="Choose a quantity"/>
             <label htmlFor="products">Products</label>
-            <button onClick={showState}>console.log all of my stuff</button>
+            {/* <button onClick={showState}>console.log all of my stuff</button> */}
             <select onChange={setProduct} className="form-control col-10" id="products">
                 <option value = '' disabled default>Please Select A Product</option>
                 {addItem(products)}
